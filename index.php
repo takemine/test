fd79459e294313022139440adb6ff06073b0e996
takeshi minemura <minemura@acts.co.jp>
2012-11-26 17:30:48 +0900

    testです

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"> 
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<script type="text/javascript">
+<!--
+	window.onload = function() {
+		var strUA = navigator.userAgent.toLowerCase();
+		if (strUA.indexOf("android") != -1) {
+			location.href = "oymjk010.php";
+		}
+		else{
+			location.href = "oymsv000.php";
+		}
+	}
+-->
+	</script>	
+	<title></title>
+</head>
+<body>
+	redirect...
+</body>
+</html>
